19fafd0e3e83a206295ddd0a7bae76cf6786e433
DENNIS <deniskiprono53@gmail.com>
2024-03-18 11:43:30 +0300

    6. Lists of lists = Matrix mandatory
    Write a function that prints a matrix of integers.
    
        Prototype: def print_matrix_integer(matrix=[[]]):
        Format: see example
        You are not allowed to import any module
        You can assume that the list only contains integers
        You are not allowed to cast integers into strings
        You have to use str.format() to print integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for list in matrix:
         for int in list:
-            if list.index(int) < (len(list) -1):
+            if list.index(int) < (len(list) - 1):
                 print("{:d}".format(int), end=" ")
             else:
-                print("{:d}".format(int), end="")
+                print("{:d}".format(int))
                 print()
